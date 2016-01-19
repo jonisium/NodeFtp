a9095050948a797f829bb17b90dd92487e6c88a4
jocaisip@outlook.com <jocaisip@outlook.com>
2016-01-19 01:59:50 -0500

    added html

--- NodeFtp/routes/index.js
+++ NodeFtp/routes/index.js
@@ -3,8 +3,65 @@
  * GET home page.
  */
 
+var cli = "ls";
+
+Array.prototype.unique = function() {
+    var o = {}, i, l = this.length, r = [];
+    for(i=0; i<l;i+=1) o[this[i]] = this[i];
+    for(i in o) r.push(o[i]);
+    return r;
+};
+function run_cmd(cmd, args, callBack ) {
+    var spawn = require('child_process').spawn;
+    var child = spawn(cmd, args);
+    var resp = "";
+
+    child.stdout.on('data', function (buffer) { resp += buffer.toString() });
+    child.stdout.on('end', function() { callBack (resp) });
+} // ()
+
+var runShell= function (cli, callback)
+{
+   var ret =[];
+   run_cmd(cli, ["-l"], function(text) {
+   var arr = text.split('\n');
+
+   arr.forEach(function(f){
+      var f1 = f.replace(/\s+/g,' ') ;
+       var fs = f1.split(' ');
+/*      if(fs.length < 3)
+        { 
+        
+         }
+      else
+        {
+           if(f1.substring(0,1) == "d")
+             {
+                  f1=f.replace(fs[8], '<a href="?folder=' + fs[8] + '" >' + fs[8] + '</a>');
+              }
+             else
+             {
+                 f1 = f.replace(fs[8],'<a href="' +  fs[8] + '" >' + fs[8] + '</a>');
+              }
+          } */
+        ret.push(f) ;
+      });
+     callback(null,ret);
+   });
+
+
+}
+
+
+
+
 exports.index = function (req, res) {
-    res.render('index', { title: 'Express', year: new Date().getFullYear() });
+     runShell("ls",function(err, data)
+     {
+       
+             res.render('index', { title: 'Express', data:data});
+     })
+
 };
 
 exports.about = function (req, res) {
